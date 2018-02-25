9ea0f01ebceff1da0b036580f2186baa89740e90
vranceanum <markdevstudio@gmail.com>
2018-02-25 19:09:25 +0200

    end-front page

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  icons = [];
   constructor() { }
 
   ngOnInit() {
+    this.icons = [
+     {icon:"fab fa-vuejs", margin:"20px"},
+     {icon:"fab fa-react" ,margin:"20px"},
+     {icon:"fab fa-css3-alt",margin:"20px"},
+     {icon:"fab fa-sass",margin:"20px"},
+     {icon:"fab fa-angular",margin:"20px"},
+     {icon:"fab fa-js",margin:"20px"}
+    ]
   }
 
 }
