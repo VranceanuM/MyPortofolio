049bb48fb93d49df06abb650c196b69b6c5f21cf
vranceanum <markdevstudio@gmail.com>
2018-02-27 06:20:44 +0200

    working with bootstrap section

--- src/app/pages/page1/page1.component.ts
+++ src/app/pages/page1/page1.component.ts
@@ -14,7 +14,10 @@ cards = [];
  this.cards = [
    {img:"../../../assets/spacex.png",link:"http://spacex.ultimatefreehost.in/?i=2"},
    {img:"../../../assets/hard.png",link:"http://hardware.ultimatefreehost.in/?i=1"},
-   {img:"../../../assets/luxe.png",link:"http://luxor.ultimatefreehost.in/?i=1"}
+   {img:"../../../assets/luxe.png",link:"http://luxor.ultimatefreehost.in/?i=1"},
+   {img:"../../../assets/portofolio.png",link:"http://profilemark.ultimatefreehost.in/?i=2"},
+   {img:"../../../assets/idea.png",link:"http://ideaslab.ultimatefreehost.in/?i=1"},
+   {img:"../../../assets/dasha1.png",link:"http://templates.ultimatefreehost.in/"}
  ]
   }
 
